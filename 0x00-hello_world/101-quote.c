b239167e065c6a06931c3500ec360b4466cd78d3
Albert <linkipskirui@gmail.com>
2023-02-16 18:46:36 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints a line to the standard error
+ * main - Program that prints a line to the standard error
  * Return: (1) Success
  */
 int main(void)
