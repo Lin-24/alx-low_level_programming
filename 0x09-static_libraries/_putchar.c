5450341a62048afb4d32a9d4e36f850d313049bc
Albert <linkipskirui@gmail.com>
2023-03-10 11:20:54 -0800

    Code output fail

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,4 @@
 #include <unistd.h>
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -13,5 +8,5 @@
  */
 int _putchar(char c)
 {
- return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
